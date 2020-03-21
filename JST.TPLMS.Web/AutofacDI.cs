b338385aa400a53aba932657f1f2cdea88355c78
zkx <zkx@ZKX73BE>
2020-03-22 07:33:44 +0800

    截至2.1.4

---
+++ JST.TPLMS.Web/AutofacDI.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace JST.TPLMS.Web
+{
+    public class AutofacDI:Autofac.Module
+    {
+        //重写Autofac管道的Load方法，在这里注册注入
+        protected override void Load(ContainerBuilder builder)
+        {
+            //注册服务的对象，这里以命名空间名称中含有JST.TPLMS.Service和Repository字符串为标志，否则注册失败
+            builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(AuthoriseService))).Where(u => u.Namespace == "JST.TPLMS.Service");
+            builder.RegisterAssemblyTypes(GetAssemblyByName("JST.TPLMS.Repository")).Where(a => a.Namespace.EndsWith("Repository")).AsImplementedInterfaces();
+        }
+        /// <summary>
+        /// 根据程序集名称获取程序集
+        /// </summary>
+        /// <param name="AssemblyName">程序集名称</param>
+        /// <returns></returns>
+        public static Assembly GetAssemblyByName(String AssemblyName)
+        {
+            return Assembly.Load(AssemblyName);
+        }
+    }
+}
--- JST.TPLMS.Web/Startup.cs
+++ JST.TPLMS.Web/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using JST.TPLMS.DataBase;
 using Microsoft.EntityFrameworkCore;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 
 namespace JST.TPLMS.Web
 {
@@ -24,23 +26,24 @@ namespace JST.TPLMS.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        //运行时将调用此方法。 使用此方法将服务添加到容器。
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                //this lambda determines whether user consent for nonessential cookies is needed for a given request 
+                //此lambda决定给定请求是否需要用户同意非必要的cookie
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             services.AddDbContext<TPLMSDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("TPLMSDbContext")));
-            services.AddMvc();
-
-            //services.Configure<CookiePolicyOptions>(options =>
-            //{
-            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-            //    options.CheckConsentNeeded = context => true;
-            //    options.MinimumSameSitePolicy = SameSiteMode.None;
-            //});
-
-
-            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            return RegisterAutofac(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //运行时将调用此方法。 使用此方法配置HTTP请求管道
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -63,6 +66,29 @@ namespace JST.TPLMS.Web
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        private IServiceProvider RegisterAutofac(IServiceCollection services)
+        {
+            //实例化Auto发出容器
+            var builder = new ContainerBuilder();
+            //将Services中的服务填充到Autofac中
+            builder.Populate(services);
+            //新模块组件注册
+            builder.RegisterModule<AutofacDI>();
+            //首先注册options,供DbContext服务初始化使用
+            builder.Register(c =>
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<TPLMSDbContext>();
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("TPLMSDbContext"));
+                return optionsBuilder.Options;
+            }).InstancePerLifetimeScope();
+            //注册DbContext
+            builder.RegisterType<TPLMSDbContext>().AsSelf().InstancePerLifetimeScope();
+            //创建容器
+            var Container = builder.Build();
+            //第三方IoC接管Core内置DI容器
+            return new AutofacServiceProvider(Container);
         }
     }
 }
